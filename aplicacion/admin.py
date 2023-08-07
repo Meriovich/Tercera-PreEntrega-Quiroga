1396cc186fed404fcc77df2412004c8df79faedc
Meriovich <marcos.arcy9842@hotmail.com>
2023-08-07 20:25:44 -0300

    Modificacion de admin.py

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Actividad, Usuario, Staff
+from .models import Usuario, Moderador, Juego
 
 # Register your models here.
-admin.site.register(Actividad)
-admin.site.register(Usuario)
-admin.site.register(Staff)
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'correo', 'edad')
+
+@admin.register(Moderador)
+class ModeradorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'correo', 'area_responsabilidad')
+
+@admin.register(Juego)
+class JuegoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'genero', 'plataforma')
